a6295da1eab1df3551d1165193a0beeeaee5c4bb
Renan Sies <renansies@gmail.com>
2018-04-02 08:18:41 -0300

    Consumes SWAPI

--- src/main/java/com/b2w/util/SWAPI.java
+++ src/main/java/com/b2w/util/SWAPI.java
@@ -1,12 +1,31 @@
 package com.b2w.util;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 public class SWAPI {
 
 	public static Integer getQuantidadeFilmes(String nome) {
-	RestTemplate restTemplate = new RestTemplate();
-    SWAPIPlanet planet = restTemplate.getForObject("http://swapi.co/api/planets/?name="+nome, SWAPIPlanet.class);
-    return planet.getFilmes().size();
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+		HttpHeaders headers = new HttpHeaders();
+	    headers.add("User-Agent", "swapi-Java-SWAPI-JAVA");
+	    HttpEntity<List<SWAPIPlanet>> httpEntity = new HttpEntity<List<SWAPIPlanet>>(headers);
+		ResponseEntity<SWAPIPlanet[]> response = restTemplate.exchange("https://swapi.co/api/planets/", HttpMethod.GET, httpEntity, SWAPIPlanet[].class, new HashMap<String, String>());
+		//SWAPIPlanet planet = response.getBody();
+		List<SWAPIPlanet> planets = new ArrayList<SWAPIPlanet>();
+		for (SWAPIPlanet swapiPlanet : response.getBody()) {
+			planets.add(swapiPlanet);
+		}
+		System.out.println(planets.get(0).getName());
+	    return null;
 	}
 }
--- src/main/java/com/b2w/util/SWAPIPlanet.java
+++ src/main/java/com/b2w/util/SWAPIPlanet.java
@@ -3,22 +3,146 @@ package com.b2w.util;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SWAPIPlanet {
 
-	private String name;
-	private List<String> filmes;
+	public String name;
+    public String diameter;
+    public String gravity;
+    public String population;
+    public String climate;
+    public String terrain;
+    public String created;
+    public String edited;
+    public String url;
+
+    @JsonProperty("rotation_period")
+    public String rotationPeriod;
+
+    @JsonProperty("orbital_period")
+    public String orbitalPeriod;
+
+    @JsonProperty("surface_water")
+    public String surfaceWater;
+
+    @JsonProperty("residents")
+    public List<String> residentsUrls;
+
+    @JsonProperty("films")
+    public List<String> filmsUrls;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public List<String> getFilmes() {
-		return filmes;
+
+	public String getDiameter() {
+		return diameter;
 	}
-	public void setFilmes(List<String> filmes) {
-		this.filmes = filmes;
+
+	public void setDiameter(String diameter) {
+		this.diameter = diameter;
 	}
+
+	public String getGravity() {
+		return gravity;
+	}
+
+	public void setGravity(String gravity) {
+		this.gravity = gravity;
+	}
+
+	public String getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(String population) {
+		this.population = population;
+	}
+
+	public String getClimate() {
+		return climate;
+	}
+
+	public void setClimate(String climate) {
+		this.climate = climate;
+	}
+
+	public String getTerrain() {
+		return terrain;
+	}
+
+	public void setTerrain(String terrain) {
+		this.terrain = terrain;
+	}
+
+	public String getCreated() {
+		return created;
+	}
+
+	public void setCreated(String created) {
+		this.created = created;
+	}
+
+	public String getEdited() {
+		return edited;
+	}
+
+	public void setEdited(String edited) {
+		this.edited = edited;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getRotationPeriod() {
+		return rotationPeriod;
+	}
+
+	public void setRotationPeriod(String rotationPeriod) {
+		this.rotationPeriod = rotationPeriod;
+	}
+
+	public String getOrbitalPeriod() {
+		return orbitalPeriod;
+	}
+
+	public void setOrbitalPeriod(String orbitalPeriod) {
+		this.orbitalPeriod = orbitalPeriod;
+	}
+
+	public String getSurfaceWater() {
+		return surfaceWater;
+	}
+
+	public void setSurfaceWater(String surfaceWater) {
+		this.surfaceWater = surfaceWater;
+	}
+
+	public List<String> getResidentsUrls() {
+		return residentsUrls;
+	}
+
+	public void setResidentsUrls(List<String> residentsUrls) {
+		this.residentsUrls = residentsUrls;
+	}
+
+	public List<String> getFilmsUrls() {
+		return filmsUrls;
+	}
+
+	public void setFilmsUrls(List<String> filmsUrls) {
+		this.filmsUrls = filmsUrls;
+	}
+    
 }
